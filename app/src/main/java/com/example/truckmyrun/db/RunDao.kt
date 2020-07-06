f1089c1b7804392a29b60ee5fd492fdc887240ec
kenei <kevinkenei9@gmail.com>
2020-07-06 17:18:06 +0300

    hilt for dependency injection

---
+++ app/src/main/java/com/example/truckmyrun/db/RunDao.kt
@@ -0,0 +1,30 @@
+package com.example.truckmyrun.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+    @Delete
+    suspend fun delete(run: Run)
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate():LiveData<List<Run>>
+    @Query("SELECT * FROM running_table ORDER BY timeInMillis DESC")
+    fun getAllRunsSortedByTimeInMillis():LiveData<List<Run>>
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedCaloriesBurned():LiveData<List<Run>>
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunsSortedByAvgSpeed():LiveData<List<Run>>
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance():LiveData<List<Run>>
+    @Query("SELECT SUM(timeinmillis) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned():LiveData<Int>
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance():LiveData<Int>
+    @Query("SELECT AVG(avgSpeedInKMH) FROM running_table")
+    fun getAvarageSpeed():LiveData<Float>
+}
---
+++ app/src/main/java/com/example/truckmyrun/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.truckmyrun.other
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
