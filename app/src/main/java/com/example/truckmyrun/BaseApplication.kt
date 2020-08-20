ed10d04e3374da246154b269200d37d66a21685b
kenei <kevinkenei9@gmail.com>
2020-08-20 12:51:51 +0300

    adding notification

--- app/src/main/java/com/example/truckmyrun/BaseApplication.kt
+++ app/src/main/java/com/example/truckmyrun/BaseApplication.kt
@@ -8,6 +8,7 @@ import timber.log.Timber
 class BaseApplication:Application() {
     override fun onCreate() {
         super.onCreate()
-        Timber.plant()
-    }
+        if (BuildConfig.DEBUG){
+        Timber.plant(Timber.DebugTree())
+    }}
 }
